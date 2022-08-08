15a184615828ff249836476e2faa9a4c913fe9cc
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-08-08 15:38:18 +0100

    index javascript file which imports other js modules

--- index.js
+++ index.js
@@ -2,61 +2,51 @@ import BookCollection from './modules/books.js';
 // import {newBook} from './modules/newBook.js';
 import getDate from './modules/date.js';
 import { showList, addBooks, contactPages } from './modules/switchPage.js';
-const form = document.querySelector('form')
-const showBooks = document.querySelector('#book-list')
-const contactPage = document.getElementById('contact-link')
-const add = document.getElementById('add-link')
-const list = document.getElementById('list-link')
-const allBooks = document.getElementById('all-books')
 
+const form = document.querySelector('form');
+const showBooks = document.querySelector('#book-list');
+const contactPage = document.getElementById('contact-link');
+const add = document.getElementById('add-link');
+const list = document.getElementById('list-link');
+// const allBooks = document.getElementById('all-books');
 
-const newBook = new BookCollection()
-console.log(newBook)
-console.log('habeeb')
+const newBook = new BookCollection();
 
-getDate()
-newBook.loadData()
+getDate();
+newBook.loadData();
 
 const addBook = (e) => {
-
-    let id = e.target.id
-    if (e.target.classList.contains('remove')) {
-        // This only removes the value from the ui not from the array
-        // e.target.parentElement.remove()
-        // Splice removes the value from the array but mutates the array in the process 
-        newBook.books.splice(id, 1);
-        newBook.display(newBook.books)
-        newBook.setData(newBook.books)
-        // console.log(books)
-    }
-}
+  const { id } = e.target;
+  if (e.target.classList.contains('remove')) {
+    // This only removes the value from the ui not from the array
+    // e.target.parentElement.remove()
+    // Splice removes the value from the array but mutates the array in the process
+    newBook.books.splice(id, 1);
+    newBook.display(newBook.books);
+    newBook.setData(newBook.books);
+    // console.log(books)
+  }
+};
 
 const removeBook = (e) => {
-    e.preventDefault()
-    const book = {
-        name : form.title.value,
-        author : form.author.value,   
-    }
-    newBook.books.push(book)
-    newBook.display(newBook.books)
-    newBook.setData(newBook.books)
-    form.title.value = ''
-    form.author.value = ''
-    console.log(newBook.books)
-}
+  e.preventDefault();
+  const book = {
+    name: form.title.value,
+    author: form.author.value,
+  };
+  newBook.books.push(book);
+  newBook.display(newBook.books);
+  newBook.setData(newBook.books);
+  form.title.value = '';
+  form.author.value = '';
+};
 
-showBooks.addEventListener('click', (e) => addBook(e))
+showBooks.addEventListener('click', (e) => addBook(e));
 
-form.addEventListener('submit', (e) => removeBook(e))
+form.addEventListener('submit', (e) => removeBook(e));
 
-list.addEventListener('click', () => showList())
+list.addEventListener('click', () => showList());
 
-add.addEventListener('click', () => addBooks())
+add.addEventListener('click', () => addBooks());
 
-contactPage.addEventListener('click', () => contactPages())
-
-
-
-
-
-
+contactPage.addEventListener('click', () => contactPages());
